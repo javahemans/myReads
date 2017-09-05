d0eab132441dd8a85467b9fe0cb6136264432641
Nagib Tharani <ntharani@gmail.com>
2017-09-05 15:52:03 -0700

    Refactor Bookshelf to functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,30 +1,26 @@
 import React from 'react'
 import Book from './Book'
 
-
-class BookShelf extends React.Component {
+const BookShelf = (props) => {
   
-    render() {
-      const books = this.props.books
-      // console.log("Books in Bookshelf, ", books)
-      return(
-        <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {/* Refactored to Book.js */}
-            {books.filter(book => book.shelf === this.props.shelfName )
-              .map((book) => 
-              <li key={book.id}>                  
-              <Book bookDetail={book} changeShelf={this.props.onUpdateShelf}/>
-              </li>
-            )}                
-          </ol>
-        </div>
+  const { books, shelfName, shelfTitle, onUpdateShelf } = props
+  // console.log("Books in Bookshelf, ", books)
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{shelfTitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {/* Refactored to Book.js */}
+          {books.filter(book => book.shelf === shelfName )
+            .map((book) => 
+            <li key={book.id}>                  
+            <Book bookDetail={book} changeShelf={onUpdateShelf}/>
+            </li>
+          )}                
+        </ol>
       </div>
-  
-      )
-    }
-  }
-  
-  export default BookShelf
+    </div>  
+  )
+}
+
+export default BookShelf
